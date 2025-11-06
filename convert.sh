f6a779645bf728511636a983fec3a53e7487f5b0
Tianle Yu <yulele28@126.com>
2025-11-06 03:06:15 -0800

    [feat] cli arg check

--- convert.sh
+++ convert.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -exo pipefail
+set -eo pipefail
+
+if [[ $# -ne 3 ]]; then
+  echo "Usage: $0 <input.pdf> <task_name> <output_dir>"
+  echo "Example: $0 presentation.pdf presentation output"
+  exit 1
+fi
 
 IMAGE_DIR=${2:-./Slides}
 rm -rf $IMAGE_DIR
